35709725b122ff7e5c6a80812331d663216b8e6c
Justin Snyder <53312867+SnyderJA@users.noreply.github.com>
2024-12-05 20:08:35 -0500

    Initial Commit

---
+++ test-app/test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_appApp.swift
+//  test-app
+//
+//  Created by Justin Snyder on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ test-appTests/test_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  test_appTests.swift
+//  test-appTests
+//
+//  Created by Justin Snyder on 12/5/24.
+//
+
+import Testing
+@testable import test_app
+
+struct test_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
